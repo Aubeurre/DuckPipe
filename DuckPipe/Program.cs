6cf4d33b2d373c22008c94db9427eb28aec6040c
Aubeurre <al.wojtkow@gmail.com>
2025-09-28 18:11:13 +0200

    HotFix: - update version in program.cs

--- DuckPipe/Program.cs
+++ DuckPipe/Program.cs
@@ -9,7 +9,7 @@ namespace DuckPipe
         ///  The main entry point for the application.
         /// </summary>
         /// 
-        public static readonly string CurrentVersion = "1.9.1"; //fix
+        public static readonly string CurrentVersion = "1.9.2"; //fix
 
         [STAThread]
         static void Main()
